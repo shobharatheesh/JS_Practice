65fbd1f016cc980df908e75d3212e0ff40f41716
Shobha Rao <sraork@gmail.com>
2026-01-07 12:38:00 +0530

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,13 @@
+//let fullName = "Shobha.Rao";
+//let age = 24;
+//let totalPrice = 1000;
+
+//console.log(fullName);
+//console.log(age);
+//console.log(totalPrice);
+
+//let age = 24;
+//age = 55;
+//age =89;
+
+//console.log(age);
---
+++ first.js
@@ -0,0 +1,26 @@
+console.log("Welcome to Learning Javascript");
+console.log("Shobha Rao");
+console.log("I love javaScript!");
+
+fullName = "Shobha Rao";
+console.log(fullName);
+
+age = 24;
+console.log(age);
+
+price = 33.15
+console.log(price);
+
+fullname = "Shobha.Rao";
+fullName = "Shubha";
+FULLNAME= "Ratheesh";
+_fullName = "Shobha.rao";
+$fullName = "Shobha Rao";
+
+
+console.log(fullname);
+console.log(fullName);
+console.log(FULLNAME);
+console.log(_fullName);
+console.log($fullName);
+
