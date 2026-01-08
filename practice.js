9fa410a8bf20a1231ab8cf7c4fbc81517490354d
Shobha Rao <sraork@gmail.com>
2026-01-08 16:41:24 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,8 @@
+const product = {
+    productName :"Ball pen",
+    rating:4,
+    offer :5,
+    price : 270,
+};
+
+console.log(product);
---
+++ practice1.js
@@ -0,0 +1,11 @@
+const profile = {
+
+    userName: "Shobha.Rao",
+    isFollow: false,
+    followers: 123,
+    following: 123,
+
+};
+
+console.log(profile);
+console.log(typeof profile["userName"]);    
