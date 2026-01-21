3a53d07e2ad84057617b7bfb56a3d2be7561646b
Shobha Rao <sraork@gmail.com>
2026-01-21 13:05:07 +0530

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,41 @@
+/*for(let i=1 ; i<=5; i++)
+{
+    console.log(i);
+}
+
+
+
+
+const arr = [10,20,30];
+
+for(const value of arr)
+{
+    console.log(value);
+}
+
+
+const user = { 
+        name:"Shobha",
+        role:"QA"
+    };
+
+for(const key in user)
+{
+    console.log(key, user[key]);
+}
+*/
+
+
+
+
+
+
+let fullName =  prompt("enter the fullname without spaces");
+
+console.log(fullName);
+
+let username = "@" + fullName + fullName.length;
+
+console.log(username);
+
+
--- Practice2.js
+++ Practice2.js
@@ -1,2 +1,53 @@
-<script src= "filename.js"></script>
+//<script src= "filename.js"></script> //embed or link the js code to html file
+
+/*  var a;
+    let b;
+    const c =10;
+  console.log(a); */     //undefined
+
+
+  /*let a;  //declaring
+  a = 10; //initializing */
+
+
+ // let name = "Shobha";
+  //name ="Testuser";
+ // console.log(name); 
+  
+  /*if(true)
+  {
+    let x= 10;
+    console.log(x);
+  }
+  
+
+
+  const country = "India";
+  //country = "USA";
+  console.log(country); */
+
+
+  //implicit type conversion
+
+ /* let result = "5" +2 ;
+  console.log(result);
+
+  console.log(typeof result);
+
+
+  let result  = "10" -5;
+  console.log(result); 
+
+
+
+  let num = Number("5");
+  console.log(num);
+  console.log(typeof num); 
+
+
+let str = String(100);
+console.log(str);
+console.log(typeof str); */
+
++, -,*,/,%,**   //Arithmetic operators
 
